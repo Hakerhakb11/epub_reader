a17bd02216d137a9e66a95c2a55118efd948bad4
Hakerhakb11 <igorshilov2007@gmail.com>
2026-04-15 14:01:34 +0600

    feat: Words couter

--- views.py
+++ views.py
@@ -1,9 +1,27 @@
 from app import app
 from flask import render_template, request
+from bs4 import BeautifulSoup
+
+
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
     user_text = ""
+    count = 0
     if request.method == 'POST':
         user_text = request.form.get('content')
-    return render_template('index.html', text=user_text)
+        if user_text:
+            soup = BeautifulSoup(user_text, 'lxml') 
+
+            if soup.head:
+                soup.head.decompose()
+
+            clean_text = soup.get_text(separator=' ')
+
+            words = clean_text.split()
+            count = len(words)
+            print(f"Количество слов: {count}")
+            print("user_text", user_text)
+            print("SOUP", soup)
+
+    return render_template('index.html', user_text=user_text, words_count=count)

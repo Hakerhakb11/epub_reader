889266c2178ca0dfc80f4b131ba304325840de47
Hakerhakb11 <igorshilov2007@gmail.com>
2026-05-12 15:23:05 +0600

    fix: handle errors of type of file

--- views.py
+++ views.py
@@ -7,6 +7,7 @@ from ebooklib import epub
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    error = None
     user_text = ""
     count = 0
     if request.method == 'POST':
@@ -27,10 +28,21 @@ def index():
                 print("SOUP", soup)
 
         elif 'content_file' in request.files:
-            user_file = request.files.get('fontent_file')
-            efile = epub.read_epub(user_file)
-            print(efile)
-            print(efile.get_items_of_type(ebooklib.ITEM_UNKNOWN))
-            print("EAZY")
+            user_file = request.files.get('content_file')
+            efile = ''
+            try:
+                efile = epub.read_epub(user_file)
+                print(efile.get_metadata('DC', 'title'))
+                print(efile.get_items_of_type(ebooklib.ITEM_DOCUMENT))
+                temp = list(efile.get_items_of_type(ebooklib.ITEM_DOCUMENT))
+                print("SPINE: \n", efile.spine, "\n\n ENDSPINE\n")
+                # for obj in temp:
+                #     print(obj)
+                print(temp)
 
-    return render_template('index.html', user_text=user_text, words_count=count)
+                print("EAZY end")
+            except:
+                error = "Error. Incorrect file type"
+                print(error)
+
+    return render_template('index.html', user_text=user_text, words_count=count, error=error)

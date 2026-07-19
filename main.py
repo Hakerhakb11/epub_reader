08791017fd3c990f1c8a18bc7bac63042fcff768
Hakerhakb11 <igorshilov2007@gmail.com>
2026-07-19 22:51:31 +0600

    feat: two run mode: desktop and dev'(#29)
    * chore: incapsulate func get_port()
    
    * chore: add --desktop group
    
    * feat: add two mode: desktop and develop
    
    * chore: add pyinstaller to desktop group

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
-from livereload import Server
+import sys
 
 from app import app, db
+from utils.server_helpers import get_port
+
+
+def start_flask(port):
+    app.run(host='127.0.0.1', port=port, debug=False, use_reloader=False)
+
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.debug = True
-    server = Server(app.wsgi_app)
-    server.watch('templates/')
-    server.watch('static/')
-    server.serve(port=5000)
+
+    port = get_port(5000)
+    if '--dev' in sys.argv:
+        from livereload import Server
+        app.debug = True
+        server = Server(app.wsgi_app)
+        server.watch('templates/')
+        server.watch('static/')
+        server.serve(port)
+
+    else:
+        import threading
+
+        import webview
+        flask_thread = threading.Thread(target=start_flask, args=(port,))
+        flask_thread.daemon = True
+        flask_thread.start()
+
+        webview.create_window(
+            'Epub reader', f'http://127.0.0.1:{port}', width=1024, height=768)
+        webview.start()
